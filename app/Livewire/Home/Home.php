daef721278f4e10d6112407c606c8255f787478f
safwan <safwanazman017@gmail.com>
2023-10-09 11:16:44 +0800

    add dashboard page

--- app/Livewire/Home/Home.php
+++ app/Livewire/Home/Home.php
@@ -3,11 +3,18 @@
 namespace App\Livewire\Home;
 
 use Livewire\Component;
+use App\Models\Ref\RefBank;
+use Livewire\WithPagination;
 
 class Home extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.home.home')->extends('layouts.main');
+        $data = RefBank::paginate(3);
+        return view('livewire.home.home',[
+            'data' => $data
+        ])->extends('layouts.main');
     }
 }
