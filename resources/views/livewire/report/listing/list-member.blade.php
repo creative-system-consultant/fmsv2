ad39f85baa487945b2f4fd2b6cd524f689858043
safwan <safwanazman017@gmail.com>
2023-10-11 15:41:41 +0800

    add loading refund advance

--- resources/views/livewire/report/listing/list-member.blade.php
+++ resources/views/livewire/report/listing/list-member.blade.php
@@ -1,17 +1,19 @@
 <div>
     <x-container title="Member Report" routeBackBtn="{{route('report.report-list')}}" titleBackBtn="report list" disableBackBtn="true">
-        <div class="grid grid-cols-1">
+        <div class="grid grid-cols-1 relative">
             <x-card title="List of Member Report">
-                <div class="flex items-center space-x-2 mb-4">
-                    <x-input 
-                        label="Start Date" 
-                        wire:model="startDate"
-                        type="date"
+                <div class="flex items-center space-x-2 mb-4 w-full">
+                    <x-datetime-picker
+                        label="Start Date"
+                        wire:model.live="startDate"
+                        without-time=true
+                        display-format="DD/MM/YYYY"
                     />
-                    <x-input 
-                        label="End Date" 
-                        wire:model="endDate"
-                        type="date"
+                    <x-datetime-picker
+                        label="End Date"
+                        wire:model.live="endDate"
+                        without-time=true
+                        display-format="DD/MM/YYYY"
                     />
                     <div class="mt-7">
                         <x-button 
