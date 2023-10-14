99bb2e84efa6cca3af85809303e3cb07bfdad0e9
safwan <safwanazman017@gmail.com>
2023-10-15 03:16:01 +0800

    fix layout refund advance & misc out

--- resources/views/livewire/teller/miscellaneous-out/miscellaneous-out-create.blade.php
+++ resources/views/livewire/teller/miscellaneous-out/miscellaneous-out-create.blade.php
@@ -1,75 +1,82 @@
 <div>
-    <x-container title="MISCELLANEOUS OUT" routeBackBtn="{{route('teller.teller-list')}}" titleBackBtn="miscellaneous out list " disableBackBtn="true">
-        <div class="grid grid-cols-1">
-            <div class="w-full p-4 bg-white border rounded-lg shadow-md dark:bg-gray-800 dark:text-gray-400 dark:border-gray-700">
-                <div class="flex flex-col items-start w-full space-x-0 space-y-2 md:flex-row md:items-center md:space-x-2 md:space-y-0">
-                    <div class="w-full md:w-96">
-                        <x-input
-                            label="Name :"
-                            wire:model="name"
-                            disabled
-                        />
-                    </div>
-                    <div class="w-full md:w-64">
-                        <x-input
-                            label="Membership No :"
-                            wire:model="refNo"
-                            disabled
-                        />
-                    </div>
-                    <div class="w-full md:w-64">
-                        <x-inputs.currency
-                            class="!pl-[2.5rem]"
-                            label="Total :"
-                            prefix="RM"
-                            thousands=","
-                            decimal="."
-                            wire:model="miscAmt"
-                            disabled
-                        />
-                    </div>
-                </div>
+    <div class="grid grid-cols-1">
+        <div class="w-full p-4 bg-white border rounded-lg shadow-md dark:bg-gray-800 dark:text-gray-400 dark:border-gray-700">
+            <div class="flex justify-end items-center w-full">
+                <x-button 
+                    xs
+                    outline 
+                    black
+                    icon="arrow-left" 
+                    label="Miscellaneous Out List" 
+                />
             </div>
-
-            <div class="grid grid-cols-12 gap-6 p-4 mt-4 rounded-lg dark:bg-gray-900" x-data="{ tab: 0 }">
-                <div class="col-span-12 lg:col-span-4 xxl:col-span-4">
-                    <x-card title="Category">
-                        <x-tab.basic-title name="0">
-                            <x-icon name="credit-card" class="w-6 h-6 mr-2"/>
-                            Contribution
-                        </x-tab.basic-title>
-                        <x-tab.basic-title name="1">
-                            <x-icon name="cash" class="w-6 h-6 mr-2"/>
-                            Members
-                        </x-tab.basic-title>
-                        <x-tab.basic-title name="2">
-                            <x-icon name="cash" class="w-6 h-6 mr-2"/>
-                            Financing
-                        </x-tab.basic-title>
-                        <x-tab.basic-title name="3">
-                            <x-icon name="cash" class="w-6 h-6 mr-2"/>
-                            Share
-                        </x-tab.basic-title>
-                    </x-card>
+            <div class="flex flex-col items-start w-full space-x-0 space-y-2 md:flex-row md:items-center md:space-x-2 md:space-y-0">
+                <div class="w-full md:w-96">
+                    <x-input
+                        label="Name :"
+                        wire:model="name"
+                        disabled
+                    />
                 </div>
-                <div class="col-span-12 lg:col-span-8 xxl:col-span-8">
-                    <div x-show="tab == 0">
-                        <livewire:teller.miscellaneous-out.category.contribution :mbrNo=$mbrNo :startDate=$startDate :endDate=$endDate :miscAmt=$miscAmt />
-                    </div>
-
-                    <div x-show="tab == 1">
-                        <livewire:teller.miscellaneous-out.category.members :mbrNo=$mbrNo :startDate=$startDate :endDate=$endDate :miscAmt=$miscAmt />
-                    </div>
-
-                    <div x-show="tab == 2">
-                        <livewire:teller.miscellaneous-out.category.financing :mbrNo=$mbrNo />
-                    </div>
-
-                    <div x-show="tab == 3">
-                        <livewire:teller.miscellaneous-out.category.share :mbrNo=$mbrNo :startDate=$startDate :endDate=$endDate :miscAmt=$miscAmt />
-                    </div>
+                <div class="w-full md:w-64">
+                    <x-input
+                        label="Membership No :"
+                        wire:model="refNo"
+                        disabled
+                    />
+                </div>
+                <div class="w-full md:w-64">
+                    <x-inputs.currency
+                        class="!pl-[2.5rem]"
+                        label="Total :"
+                        prefix="RM"
+                        thousands=","
+                        decimal="."
+                        wire:model="miscAmt"
+                        disabled
+                    />
                 </div>
             </div>
         </div>
-    </x-container>
+
+        <div class="grid grid-cols-12 gap-6 py-4 mt-4 rounded-lg dark:bg-gray-900" x-data="{ tab: 0 }">
+            <div class="col-span-12 lg:col-span-4 xxl:col-span-4">
+                <x-card title="Category">
+                    <x-tab.basic-title name="0">
+                        <x-icon name="credit-card" class="w-6 h-6 mr-2"/>
+                        Contribution
+                    </x-tab.basic-title>
+                    <x-tab.basic-title name="1">
+                        <x-icon name="cash" class="w-6 h-6 mr-2"/>
+                        Members
+                    </x-tab.basic-title>
+                    <x-tab.basic-title name="2">
+                        <x-icon name="cash" class="w-6 h-6 mr-2"/>
+                        Financing
+                    </x-tab.basic-title>
+                    <x-tab.basic-title name="3">
+                        <x-icon name="cash" class="w-6 h-6 mr-2"/>
+                        Share
+                    </x-tab.basic-title>
+                </x-card>
+            </div>
+            <div class="col-span-12 lg:col-span-8 xxl:col-span-8">
+                <div x-show="tab == 0">
+                    <livewire:teller.miscellaneous-out.category.contribution :mbrNo=$mbrNo :startDate=$startDate :endDate=$endDate :miscAmt=$miscAmt />
+                </div>
+
+                <div x-show="tab == 1">
+                    <livewire:teller.miscellaneous-out.category.members :mbrNo=$mbrNo :startDate=$startDate :endDate=$endDate :miscAmt=$miscAmt />
+                </div>
+
+                <div x-show="tab == 2">
+                    <livewire:teller.miscellaneous-out.category.financing :mbrNo=$mbrNo />
+                </div>
+
+                <div x-show="tab == 3">
+                    <livewire:teller.miscellaneous-out.category.share :mbrNo=$mbrNo :startDate=$startDate :endDate=$endDate :miscAmt=$miscAmt />
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
--- resources/views/livewire/teller/refund-advance/refund-advance-create.blade.php
+++ resources/views/livewire/teller/refund-advance/refund-advance-create.blade.php
@@ -1,49 +1,56 @@
 <div>
-    <x-container title="Refund Advance" routeBackBtn="{{route('teller.teller-list')}}" titleBackBtn="Refund Advance list" disableBackBtn="true">
-        <div class="grid grid-cols-1">
-            <div class="w-full p-4 bg-white border rounded-lg shadow-md dark:bg-gray-800 dark:text-gray-400 dark:border-gray-700">
-                <div class="flex flex-col items-start w-full space-x-0 space-y-2 md:flex-row md:items-center md:space-x-2 md:space-y-0">
-                    <div class="w-full md:w-96">
-                        <x-input
-                            label="Name :"
-                            wire:model="name"
-                            disabled
-                        />
-                    </div>
-                    <div class="w-full md:w-64">
-                        <x-input
-                            label="Account No :"
-                            wire:model="accountNo"
-                            disabled
-                        />
-                    </div>
-                    <div class="w-full md:w-64">
-                        <x-inputs.currency
-                            class="!pl-[2.5rem]"
-                            label="Advance Amount :"
-                            prefix="RM"
-                            thousands=","
-                            decimal="."
-                            wire:model="advAmount"
-                            disabled
-                        />
-                    </div>
-                </div>
+    <div class="grid grid-cols-1">
+        <div class="w-full p-4 bg-white border rounded-lg shadow-md dark:bg-gray-800 dark:text-gray-400 dark:border-gray-700">
+            <div class="flex justify-end items-center w-full">
+                <x-button 
+                    xs
+                    outline 
+                    black
+                    icon="arrow-left" 
+                    label="Refund Advance List" 
+                />
             </div>
-
-            <div class="grid grid-cols-12 gap-6 p-4 mt-4 rounded-lg dark:bg-gray-900" x-data="{ tab: 0 }">
-                <div class="col-span-12 lg:col-span-4 xxl:col-span-4">
-                    <x-card title="Category">
-                        <x-tab.basic-title name="0">
-                            <x-icon name="user-circle" class="w-6 h-6 mr-2"/>
-                            Pay to Members
-                        </x-tab.basic-title>
-                    </x-card>
+            <div class="flex flex-col items-start w-full space-x-0 space-y-2 md:flex-row md:items-center md:space-x-2 md:space-y-0">
+                <div class="w-full md:w-96">
+                    <x-input
+                        label="Name :"
+                        wire:model="name"
+                        disabled
+                    />
                 </div>
-                <div class="col-span-12 lg:col-span-8 xxl:col-span-8">
-                    <livewire:teller.refund-advance.category.pay-to-member :accountNo="$accountNo"/>
+                <div class="w-full md:w-64">
+                    <x-input
+                        label="Account No :"
+                        wire:model="accountNo"
+                        disabled
+                    />
+                </div>
+                <div class="w-full md:w-64">
+                    <x-inputs.currency
+                        class="!pl-[2.5rem]"
+                        label="Advance Amount :"
+                        prefix="RM"
+                        thousands=","
+                        decimal="."
+                        wire:model="advAmount"
+                        disabled
+                    />
                 </div>
             </div>
         </div>
-    </x-container>
+
+        <div class="grid grid-cols-12 gap-6 py-4 mt-4 rounded-lg dark:bg-gray-900" x-data="{ tab: 0 }">
+            <div class="col-span-12 lg:col-span-4 xxl:col-span-4">
+                <x-card title="Category">
+                    <x-tab.basic-title name="0">
+                        <x-icon name="user-circle" class="w-6 h-6 mr-2"/>
+                        Pay to Members
+                    </x-tab.basic-title>
+                </x-card>
+            </div>
+            <div class="col-span-12 lg:col-span-8 xxl:col-span-8">
+                <livewire:teller.refund-advance.category.pay-to-member :accountNo="$accountNo"/>
+            </div>
+        </div>
+    </div>
 </div>
