bdcce754e4afbabad85795645ed92045293c8f7f
Nur Laili <nurlailisam1599@gmail.com>
2023-10-18 17:56:13 +0800

    Report Summary Total Share

---
+++ app/Action/StoredProcedure/SpUpRptSummaryTotalshare.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Action\StoredProcedure;
+
+use DB;
+use Illuminate\Support\Collection;
+
+class SpUpRptSummaryTotalshare
+{
+    public static function getRawData($input)
+    {
+        return DB::select("fms.up_rpt_summary_totalshare :clientId, :startDate, :endDate", $input);
+    }
+
+    public static function formatData($data)
+    {
+        return [
+            'Membership No'  => [
+                'value' =>  $data->mbrno,
+                'align' => 'left'
+            ],
+            'Identity No' => [
+                'value' => $data->identityno,
+                'align' => 'right'
+            ],
+            'Name' => [
+                'value' => $data->mbrname,
+                'align' => 'right'
+            ],
+            'Opening' => [
+                'value' => $data->opening,
+                'align' => 'right'
+            ],
+            'Purchase' => [
+                'value' =>$data->purchase,
+                'align' => 'right'
+            ],
+            'Selling' => [
+                'value' =>$data->selling,
+                'align' => 'right'
+            ],
+            'Total Share'    => [
+                'value' => number_format($data->total_share,2),
+                'align' => 'right'
+            ],
+            'Closing' => [
+                'value' =>$data->closing,
+                'align' => 'right'
+            ],
+        ];
+    }
+
+    public static function formatDataForExcel($data)
+    {
+        $formattedData = self::formatData($data);
+        $excelData = [];
+
+        foreach ($formattedData as $column => $cell) {
+            $excelData[$column] = $cell['value'];
+        }
+
+        return $excelData;
+    }
+
+    public static function handleForExcel($input, $format = false)
+    {
+        $rawData = self::getRawData($input);
+        foreach ($rawData as $data) {
+            $formattedData = $format ? self::formatDataForExcel($data) : $data;
+            yield $formattedData;
+        }
+    }
+
+    public static function handleForTable($input, $format = false)
+    {
+        $rawData = self::getRawData($input);
+        $formattedData = [];
+        foreach ($rawData as $data) {
+            $formattedData[] = $format ? self::formatData($data) : $data;
+        }
+        return new Collection($formattedData);
+    }
+}
---
+++ app/Livewire/Report/Operation/Summary/sumtotalshare.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Livewire\Report\Operation\Summary;
+
+use App\Action\StoredProcedure\SpUpRptSummaryTotalshare;
+use App\Services\General\ReportService;
+use Livewire\Attributes\Rule;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+
+class sumtotalshare extends Component
+{
+    use WithPagination;
+    public $clientId = 1;
+
+    #[Rule('required')]
+    public $startDate;
+
+    #[Rule('required')]
+    public $endDate;
+
+    public function generateExcel()
+    {
+        $this->validate();
+        return $this->handleExcel();
+    }
+
+    private function handleDataTable()
+    {
+        $data = SpUpRptSummaryTotalshare::handleForTable([
+            'clientId' => $this->clientId,
+            'startDate' => $this->startDate,
+            'endDate' => $this->endDate,
+        ], true);
+
+        return ReportService::paginateData($data);
+    }
+
+    private function handleExcel()
+    {
+        $dataGenerator = function () {
+            $rawData = SpUpRptSummaryTotalshare::handleForExcel([
+                'clientId' => $this->clientId,
+                'startDate' => $this->startDate,
+                'endDate' => $this->endDate,
+            ], true);
+
+            foreach ($rawData as $data) {
+                yield $data;
+            }
+        };
+
+        $filename = 'SummaryTotalShare-%s.xlsx';
+        $report = new ReportService();
+
+        return $report->generateExcelReport($dataGenerator, $filename, $this->startDate);
+    }
+
+    public function render()
+    {
+        $result = null;
+
+        if ($this->startDate && $this->endDate) {
+            $result = $this->handleDataTable();
+        }
+
+        return view('livewire.report.operation.summary.sumtotalshare', [
+            'result' => $result
+        ])->extends('layouts.main');
+    }
+}
