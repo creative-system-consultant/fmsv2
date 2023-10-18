7f0f1f40fd2e272742c55846fe08f23dd4e451a2
Nur Laili <nurlailisam1599@gmail.com>
2023-10-18 17:55:37 +0800

    Report Share Withdrawal

---
+++ app/Livewire/Report/Operation/Share/sharewithdrawal.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Livewire\Report\Operation\Share;
+
+use App\Action\StoredProcedure\SpFmsUpRptWithdrawalShare;
+use App\Services\General\ReportService;
+use Livewire\Attributes\Rule;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+
+class sharewithdrawal extends Component
+{
+    use WithPagination;
+    public $clientId = 1;
+
+    #[Rule('required')]
+    public $startDate;
+
+    #[Rule('required')]
+    public $endDate;
+
+    public function generateExcel()
+    {
+        $this->validate();
+        return $this->handleExcel();
+    }
+
+    private function handleDataTable()
+    {
+        $data = SpFmsUpRptWithdrawalShare::handleForTable([
+            'clientId' => $this->clientId,
+            'startDate' => $this->startDate,
+            'endDate' => $this->endDate,
+        ], true);
+
+        return ReportService::paginateData($data);
+    }
+
+    private function handleExcel()
+    {
+        $dataGenerator = function () {
+            $rawData = SpFmsUpRptWithdrawalShare::handleForExcel([
+                'clientId' => $this->clientId,
+                'startDate' => $this->startDate,
+                'endDate' => $this->endDate,
+            ], true);
+
+            foreach ($rawData as $data) {
+                yield $data;
+            }
+        };
+
+        $filename = 'ShareWithdrawal-%s.xlsx';
+        $report = new ReportService();
+
+        return $report->generateExcelReport($dataGenerator, $filename, $this->startDate);
+    }
+
+    public function render()
+    {
+        $result = null;
+
+        if ($this->startDate && $this->endDate) {
+            $result = $this->handleDataTable();
+        }
+
+        return view('livewire.report.operation.share.sharewithdrawal', [
+            'result' => $result
+        ])->extends('layouts.main');
+    }
+}
