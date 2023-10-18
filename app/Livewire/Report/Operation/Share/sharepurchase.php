e044eda4ac72b15a5f9183472401ae86f416b3fd
Nur Laili <nurlailisam1599@gmail.com>
2023-10-18 17:53:44 +0800

    Report Share Purchase

---
+++ app/Livewire/Report/Operation/Share/sharepurchase.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Livewire\Report\Operation\Share;
+
+use App\Action\StoredProcedure\SpFmsUpRptSharePurchase;
+use App\Services\General\ReportService;
+use Livewire\Attributes\Rule;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+
+class sharepurchase extends Component
+{
+    use WithPagination;
+    public $clientId = 1;
+
+    #[Rule('required')]
+    public $startDate;
+
+    #[Rule('required')]
+    public $endDate;
+
+    public function generateExcel()
+    {
+        $this->validate();
+        return $this->handleExcel();
+    }
+
+    private function handleDataTable()
+    {
+        $data = SpFmsUpRptSharePurchase::handleForTable([
+            'clientId' => $this->clientId,
+            'startDate' => $this->startDate,
+            'endDate' => $this->endDate,
+        ], true);
+
+        return ReportService::paginateData($data);
+    }
+
+    private function handleExcel()
+    {
+        $dataGenerator = function () {
+            $rawData = SpFmsUpRptSharePurchase::handleForExcel([
+                'clientId' => $this->clientId,
+                'startDate' => $this->startDate,
+                'endDate' => $this->endDate,
+            ], true);
+
+            foreach ($rawData as $data) {
+                yield $data;
+            }
+        };
+
+        $filename = 'SharePurchase-%s.xlsx';
+        $report = new ReportService();
+
+        return $report->generateExcelReport($dataGenerator, $filename, $this->startDate);
+    }
+
+    public function render()
+    {
+        $result = null;
+
+        if ($this->startDate && $this->endDate) {
+            $result = $this->handleDataTable();
+        }
+
+        return view('livewire.report.operation.share.sharepurchase', [
+            'result' => $result
+        ])->extends('layouts.main');
+    }
+}
---
+++ resources/views/livewire/report/operation/share/sharepurchase.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <x-report title="Share Purchase" :startDate="true" :endDate="true" :reportDate="false" :result="$result"/>
+</div>
