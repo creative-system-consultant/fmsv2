f1425352a6faf19af417f88c51ec8f2f757bfc81
athrhsh <athirahhamzi04@gmail.com>
2023-10-19 10:14:22 +0800

    Update render

--- app/Livewire/Report/Operation/Financing/Disbursement.php
+++ app/Livewire/Report/Operation/Financing/Disbursement.php
@@ -4,12 +4,17 @@ namespace App\Livewire\Report\Operation\Financing;
 
 use App\Action\StoredProcedure\SpFmsFinancingDisbursement;
 use App\Services\General\ReportService;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Livewire\Attributes\Rule;
 use Livewire\Component;
+use Livewire\WithPagination;
+use WireUi\Traits\Actions;
 
 class Disbursement extends Component
 {
-    public $clientId = 1;
+    use Actions, WithPagination;
+
+    public $clientId;
 
     #[Rule('required')]
     public $startDate;
@@ -17,23 +22,67 @@ class Disbursement extends Component
     #[Rule('required')]
     public $endDate;
 
+    public function mount()
+    {
+        $this->clientId = auth()->user()->client_id;
+    }
+    
+    protected function getRawData()
+    {
+        return SpFmsFinancingDisbursement::getRawData([
+            'clientId' => $this->clientId,
+            'startDate' => $this->startDate,
+            'endDate' => $this->endDate,
+        ], true);
+    }
+
     public function generateExcel()
     {
         $this->validate();
 
-        $data = SpFmsFinancingDisbursement::handle([
-            'clientId' => $this->clientId,
-            'startDate' => $this->startDate,
-            'endDate' => $this->endDate,
-        ]);
+        $rawData = $this->getRawData();
+
+        if (count($rawData) > 0) {
+            $formattedData = [];
+            foreach ($rawData as $data) {
+                $formattedData[] = SpFmsFinancingDisbursement::formatDataForExcel($data);
+            }
+            return $this->handleExcel($formattedData);
+        } else {
+            $this->dialog()->success('Process Complete!', 'No Data Found.');
+        }
+    }
+    private function handleDataTable($rawData)
+    {
+        $data = SpFmsFinancingDisbursement::handleForTable($rawData, true);
+
+        return ReportService::paginateData($data);
+    }
+
+    private function handleExcel($rawData)
+    {
+        $dataGenerator = function () use ($rawData) {
+            foreach ($rawData as $data) {
+                yield $data;
+            }
+        };
 
         $filename = 'FinancingDisbursement-%s.xlsx';
-        $report = new ReportService(); 
+        $report = new ReportService();
 
-        return $report->generateExcelReport($data, $filename, $this->startDate);
+        return $report->generateExcelReport($dataGenerator, $filename, $this->startDate);
     }
+
     public function render()
     {
-        return view('livewire.report.operation.financing.disbursement')->extends('layouts.main');
+        $result = null;
+        $rawData = $this->getRawData();
+
+        if($this->startDate && $this->endDate && count($rawData) <= 1000){
+            $result = $this->handleDataTable($rawData);
+        }
+        return view('livewire.report.operation.financing.disbursement', [
+            'result' =>$result
+        ])->extends('layouts.main');
     }
 }
