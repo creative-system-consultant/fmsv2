70126620e0374c15598f66a0496d9c061425fb9f
Nur Laili <nurlailisam1599@gmail.com>
2023-10-24 18:30:06 +0800

    Report Details Financing Yearly

---
+++ app/Livewire/Report/Operation/Monthly/DetailsFinYrly.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Livewire\Report\Operation\Monthly;
+
+use App\Action\StoredProcedure\SpUpRptDetailsFinancingYearly;
+use App\Services\General\ReportService;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Livewire\Attributes\Rule;
+use Livewire\Component;
+use Livewire\WithPagination;
+use WireUi\Traits\Actions;
+
+class DetailsFinYrly extends Component
+{
+    use Actions, WithPagination;
+
+    public $clientId;
+
+    #[Rule('required')]
+    public $reportDate;
+
+
+    public function mount()
+    {
+        $this->clientId = auth()->user()->client_id;
+    }
+
+    protected function getRawData()
+    {
+        return SpUpRptDetailsFinancingYearly::getRawData([
+            'clientId' => $this->clientId,
+            'reportDate' => $this->reportDate,
+        ], true);
+    }
+
+    public function generateExcel()
+    {
+        $this->validate();
+
+        $rawData = $this->getRawData();
+
+        if (count($rawData) > 0) {
+            $formattedData = [];
+            foreach ($rawData as $data) {
+                $formattedData[] = SpUpRptDetailsFinancingYearly::formatDataForExcel($data);
+            }
+
+            return $this->handleExcel($formattedData);
+        } else {
+            $this->dialog()->success('Process Complete!', 'No Data Found.');
+        }
+    }
+
+    private function handleDataTable($rawData)
+    {
+        $data = SpUpRptDetailsFinancingYearly::handleForTable($rawData, true);
+
+        return ReportService::paginateData($data);
+    }
+
+    private function handleExcel($rawData)
+    {
+        $dataGenerator = function () use ($rawData) {
+            foreach ($rawData as $data) {
+                yield $data;
+            }
+        };
+
+        $filename = 'DetailsFinancingMonthly-%s.xlsx';
+        $report = new ReportService();
+
+        return $report->generateExcelReport($dataGenerator, $filename, $this->reportDate);
+    }
+
+    public function render()
+    {
+        $result = null;
+
+        if($this->reportDate) {
+            $rawData = $this->getRawData();
+
+            if(count($rawData) <= 1000) {
+                $result = $this->handleDataTable($rawData);
+            }
+        }
+
+        return view('livewire.report.operation.monthly.details-fin-yrly', [
+            'result' => $result
+        ])->extends('layouts.main');
+    }
+}
---
+++ resources/views/livewire/report/operation/monthly/details-fin-yrly.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <x-report title="Details Financing Yearly" :startDate="false" :endDate="false" :reportDate="true" :result="$result"/>
+</div>
