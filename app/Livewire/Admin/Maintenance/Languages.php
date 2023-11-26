5e1856c17583aa966fee80adc327daf5e9cc032c
Nazirul Fitri <nazirul@csc.net.my>
2023-11-26 19:59:27 +0800

    Refactor Language Maintenance

--- app/Livewire/Admin/Maintenance/Languages.php
+++ app/Livewire/Admin/Maintenance/Languages.php
@@ -3,8 +3,11 @@
 namespace App\Livewire\Admin\Maintenance;
 
 use App\Models\Ref\RefLanguage;
-use App\Services\Model\LanguageService;
+use App\Rules\Maintenance\ValidDescription;
+use App\Services\General\ModelService;
 use App\Services\General\PopupService;
+use App\Services\Maintenance\FormattingService;
+use App\Services\Maintenance\GeneralService as MaintenanceService;
 use App\Traits\MaintenanceModalTrait;
 use Livewire\Attributes\Rule;
 use Livewire\Component;
@@ -15,64 +18,73 @@ class Languages extends Component
 {
     use Actions, WithPagination,MaintenanceModalTrait;
 
-    #[Rule('required|numeric|min:1|max:9')]
-    public $institute_id;
-
-    #[Rule('required|regex:/^[A-Za-z ]+(\([A-Za-z]+\))?$/')]
-    public $description;
-
+    // Properties for modal and religion management
     public $openModal;
     public $modalTitle;
     public $modalDescription;
     public $modalMethod;
     public $language;
+
+    // Properties for language data
+    public $institute_id;
+    public $description;
+
+    // Pagination & searching
     public $paginated;
     public $searchQuery;
 
-    protected $languageService;
+    // Services
     protected $popupService;
+
+    public function rules()
+    {
+        return [
+            'institute_id' => ['required', 'numeric', 'min:1', 'max:9'],
+            'description' => ['required', new ValidDescription],
+        ];
+    }
 
     public function __construct()
     {
-        $this->languageService = new LanguageService();
         $this->popupService = app(PopupService::class);
     }
 
     public function openCreateModal()
     {
         $this->setupModal("create", "Create Language", "Language");
-        $this->reset(['description', 'institute_id']); 
-        $this->resetValidation(); 
+        $this->reset(['description', 'institute_id']);
+        $this->resetValidation();
     }
 
 
     public function openUpdateModal($id)
     {
-        $this->language = RefLanguage::find($id);
+        $this->language = ModelService::findById(RefLanguage::class, $id);
         $this->description = $this->language->description;
         $this->institute_id = $this->language->institute_id;
         $this->setupModal("update", "Update Language", "Language", "update({$id})");
         $this->resetValidation(); // Clear validation errors
     }
 
+    protected function formatData()
+    {
+        return [
+            'institute_id' => FormattingService::formatCode($this->institute_id, true, 2),
+            'description' => FormattingService::formatDescription($this->description),
+        ];
+    }
+
     public function create()
     {
-        
         $this->validate();
 
-        $paddedCode = str_pad(trim(strtoupper($this->institute_id)), 2, '0', STR_PAD_LEFT);
+        $formattedData = $this->formatData();
 
-        if (LanguageService::isInstituteIdExists($this->institute_id)) {
-            $this->addError('institute_id', 'The ID has already been taken.');
+        if (MaintenanceService::isCodeExists(RefLanguage::class, $formattedData['institute_id'], 'institute_id')) {
+            $this->addError('institute_id', 'The institute id has already been taken.');
         } else {
-            $data = [
-                'description' =>  trim(strtoupper(preg_replace('/\s+/', ' ', ($this->description)))),
-                'institute_id' => $paddedCode,
-            ];
-
-            LanguageService::createLanguage($data);
-
-            $this->reset();
+            ModelService::create(RefLanguage::class, $formattedData);
+            $this->reset('institute_id', 'description');
             $this->openModal = false;
         }
     }
@@ -81,40 +93,37 @@ class Languages extends Component
     {
         $this->validate();
 
-        $trim_code = trim($this->institute_id);
+        $formattedData = $this->formatData();
 
-        if(strlen($trim_code) == '1') {
-            $trim_code = '0' . $trim_code;
-        }
-        if (LanguageService::canUpdateInstituteId($id, $trim_code)){
-            $data = [
-                'institute_id' => $trim_code,
-                'description' => trim(strtoupper(preg_replace('/\s+/', ' ', ($this->description)))),
-            ];
-            LanguageService::updateLanguage($id, $data);
+        if (MaintenanceService::canUpdateCode(RefLanguage::class, $id, $formattedData['institute_id'], 'institute_id')) {
+            ModelService::update(RefLanguage::class, $id, $formattedData);
+            $this->reset('institute_id', 'description');
             $this->openModal = false;
         } else {
             $this->addError('institute_id', 'The institute id has already been taken.');
-    }
+        }
     }
 
-    public function delete($id,$institute_id)
+    public function delete($id, $institute_id)
     {
         $this->popupService->confirm($this, 'ConfirmDelete', 'Delete the information?', 'Are you delete the Institute ID: '.$institute_id.'?',$id);
     }
 
     public function ConfirmDelete($id)
     {
-        LanguageService::deleteLanguage($id);
+        ModelService::delete(RefLanguage::class, $id);
     }
 
     public function render()
     {
-        $data = $this->languageService->getLanguageResult($this->searchQuery, $this->paginated);
+        $data = MaintenanceService::getPaginated(
+            RefLanguage::class,
+            $this->paginated, // $perPage
+            $this->searchQuery, // $searchQuery
+        );
 
         return view('livewire.admin.maintenance.languages', [
             'data' => $data,
         ])->extends('layouts.main');
     }
 }
-
