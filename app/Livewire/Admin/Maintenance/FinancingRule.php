e7cf48b02bcca6b4f1a314cc322c82a1140d110d
Nazirul Fitri <nazirul@csc.net.my>
2023-11-26 19:16:30 +0800

    Refactor Financing Rule

--- app/Livewire/Admin/Maintenance/FinancingRule.php
+++ app/Livewire/Admin/Maintenance/FinancingRule.php
@@ -3,10 +3,12 @@
 namespace App\Livewire\Admin\Maintenance;
 
 use App\Models\Ref\RefFinancingRule;
-use App\Services\Model\FinancingRuleService;
+use App\Rules\Maintenance\ValidDescription;
+use App\Services\General\ModelService;
 use App\Services\General\PopupService;
+use App\Services\Maintenance\FormattingService;
+use App\Services\Maintenance\GeneralService as MaintenanceService;
 use App\Traits\MaintenanceModalTrait;
-use Livewire\Attributes\Rule;
 use Livewire\Component;
 use Livewire\WithPagination;
 use WireUi\Traits\Actions;
@@ -15,26 +17,34 @@ class FinancingRule extends Component
 {
     use Actions, WithPagination,MaintenanceModalTrait;
 
-    #[Rule('required|numeric|min:1|max:9999')]
-    public $code;
-
-    #[Rule('required|regex:/^[A-Za-z ]+(\([A-Za-z]+\))?$/')]
-    public $description;
-
+    // Properties for modal and financing rule management
     public $openModal;
     public $modalTitle;
     public $modalDescription;
     public $modalMethod;
     public $financingRule;
+
+    // Properties for financing rule data
+    public $code;
+    public $description;
+
+    // Pagination and searching
     public $paginated;
     public $searchQuery;
 
-    protected $financingRuleService;
+    // Services
     protected $popupService;
+
+    public function rules()
+    {
+        return [
+            'code' => ['required', 'numeric', 'min:1', 'max:9999'],
+            'description' => ['required', new ValidDescription],
+        ];
+    }
 
     public function __construct()
     {
-        $this->financingRuleService = new FinancingRuleService();
         $this->popupService = app(PopupService::class);
     }
 
@@ -47,30 +57,32 @@ class FinancingRule extends Component
 
     public function openUpdateModal($id)
     {
-        $this->financingRule = RefFinancingRule::find($id);
+        $this->financingRule = ModelService::findById(RefFinancingRule::class, $id);
         $this->description = $this->financingRule->description;
         $this->code = $this->financingRule->code;
         $this->setupModal("update", "Update Financing Rule", "Financing Rule", "update({$id})");
         $this->resetValidation(); // Clear validation errors
     }
 
+    protected function formatData()
+    {
+        return [
+            'code' => FormattingService::formatCode($this->code, true, 4),
+            'description' => FormattingService::formatDescription($this->description),
+        ];
+    }
+
     public function create()
     {
-        
         $this->validate();
 
-        $paddedCode = str_pad(trim(strtoupper($this->code)), 4, '0', STR_PAD_LEFT);
+        $formattedData = $this->formatData();
 
-        if (FinancingRuleService::isCodeExists($paddedCode)) {
+        if (MaintenanceService::isCodeExists(RefFinancingRule::class, $formattedData['code'])) {
             $this->addError('code', 'The code has already been taken.');
         } else {
-            $data = [
-                'description' => trim(preg_replace('/\s+/', ' ', strtoupper($this->description))),
-                'code' => $paddedCode,
-            ];
-
-            FinancingRuleService::createFinancingRule($data);
-            $this->reset();
+            ModelService::create(RefFinancingRule::class, $formattedData);
+            $this->reset('code', 'description');
             $this->openModal = false;
         }
     }
@@ -79,13 +91,11 @@ class FinancingRule extends Component
     {
         $this->validate();
 
-        if (FinancingRuleService::canUpdateCode($id, $this->code)) {
-            $data = [
-                'description' => trim(preg_replace('/\s+/', ' ', strtoupper($this->description))),
-                'code' => str_pad(trim(strtoupper($this->code)), 4, '0', STR_PAD_LEFT),
-            ];
+        $formattedData = $this->formatData();
 
-            FinancingRuleService::updateFinancingRule($id, $data);
+        if (MaintenanceService::canUpdateCode(RefFinancingRule::class, $id, $formattedData['code'])) {
+            ModelService::update(RefFinancingRule::class, $id, $formattedData);
+            $this->reset('code', 'description');
             $this->openModal = false;
         } else {
             $this->addError('code', 'The code has already been taken.');
@@ -96,16 +106,19 @@ class FinancingRule extends Component
     {
     $this->popupService->confirm($this, 'ConfirmDelete', 'Delete the information?', 'Are you sure you want to delete code:' . $code .'?', $id);
     }
-    
-    
+
     public function ConfirmDelete($id)
     {
-        FinancingRuleService::deleteFinancingRule($id);
+        ModelService::delete(RefFinancingRule::class, $id);
     }
 
     public function render()
     {
-        $data = $this->financingRuleService->getFinancingRuleResult($this->searchQuery, $this->paginated);
+        $data = MaintenanceService::getPaginated(
+            RefFinancingRule::class,
+            $this->paginated, // $perPage
+            $this->searchQuery, // $searchQuery
+        );
 
         return view('livewire.admin.maintenance.financing-rule', [
             'data' => $data,
