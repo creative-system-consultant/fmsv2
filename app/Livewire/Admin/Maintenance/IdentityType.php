b68548e72b79dceaa2647349a27b6b261c44dcd5
Nazirul Fitri <nazirul@csc.net.my>
2023-11-26 20:20:58 +0800

    Refactor Identity Type Maintenance

--- app/Livewire/Admin/Maintenance/IdentityType.php
+++ app/Livewire/Admin/Maintenance/IdentityType.php
@@ -3,10 +3,12 @@
 namespace App\Livewire\Admin\Maintenance;
 
 use App\Models\Ref\RefIdentityType;
-use App\Services\Model\IdentityTypeService;
+use App\Rules\Maintenance\ValidDescription;
+use App\Services\General\ModelService;
 use App\Services\General\PopupService;
+use App\Services\Maintenance\FormattingService;
+use App\Services\Maintenance\GeneralService as MaintenanceService;
 use App\Traits\MaintenanceModalTrait;
-use Livewire\Attributes\Rule;
 use Livewire\Component;
 use Livewire\WithPagination;
 use WireUi\Traits\Actions;
@@ -15,26 +17,34 @@ class IdentityType extends Component
 {
     use Actions, WithPagination,MaintenanceModalTrait;
 
-    #[Rule('required|string|min:3|max:3')]
-    public $type;
-
-    #[Rule('required|regex:/^[A-Za-z\/\s]+$/')]
-    public $description;
-
+    // Properties for modal and identity type management
     public $openModal;
     public $modalTitle;
     public $modalDescription;
     public $modalMethod;
     public $identitytype;
+
+    // Properties for identity type data
+    public $type;
+    public $description;
+
+    // Pagination & searching
     public $paginated;
     public $searchQuery;
 
-    protected $identitytypeService;
+    // Services
     protected $popupService;
+
+    public function rules()
+    {
+        return [
+            'type' => ['required', 'string', 'size:3'],
+            'description' => ['required', new ValidDescription],
+        ];
+    }
 
     public function __construct()
     {
-        $this->identitytypeService = new IdentityTypeService();
         $this->popupService = app(PopupService::class);
     }
 
@@ -42,36 +52,37 @@ class IdentityType extends Component
     {
         $this->setupModal("create", "Create Identity Type", "Identity Type");
         $this->reset(['description', 'type']); // Clear the values for description and code
-        $this->resetValidation(); // Clear validation errors    
+        $this->resetValidation(); // Clear validation errors
     }
 
     public function openUpdateModal($id)
     {
-        $this->identitytype = RefIdentityType::find($id);
+        $this->identitytype = ModelService::findById(RefIdentityType::class, $id);
         $this->description = $this->identitytype->description;
         $this->type = $this->identitytype->type;
         $this->setupModal("update", "Update Identity Type", "Identity Type", "update({$id})");
         $this->resetValidation(); // Clear validation errors
     }
 
+    protected function formatData()
+    {
+        return [
+            'type' => FormattingService::formatCode($this->type, true, 3),
+            'description' => FormattingService::formatDescription($this->description),
+        ];
+    }
+
     public function create()
     {
-        
         $this->validate();
 
-        $paddedType = str_pad(trim(strtoupper($this->type)), 3, STR_PAD_LEFT);
+        $formattedData = $this->formatData();
 
-        if (IdentityTypeService::isTypeExists($paddedType)) {
+        if (MaintenanceService::isCodeExists(RefIdentityType::class, $formattedData['type'], 'type')) {
             $this->addError('type', 'The type has already been taken.');
         } else {
-            $data = [
-                'description' => trim(preg_replace('/\s+/', ' ', strtoupper($this->description))),
-                'type' => $paddedType,
-            ];
-
-            IdentityTypeService::createIdentityType($data);
-
-            $this->reset();
+            ModelService::create(RefIdentityType::class, $formattedData);
+            $this->reset('type', 'description');
             $this->openModal = false;
         }
     }
@@ -80,32 +91,34 @@ class IdentityType extends Component
     {
         $this->validate();
 
-        if (IdentityTypeService::canUpdateType($id, $this->type)) {
-            $data = [
-                'description' => trim(preg_replace('/\s+/', ' ', strtoupper($this->description))),
-                'type' => str_pad(trim(strtoupper($this->type)), 3, STR_PAD_LEFT),
-            ];
+        $formattedData = $this->formatData();
 
-            IdentityTypeService::updateIdentityType($id, $data);
+        if (MaintenanceService::canUpdateCode(RefIdentityType::class, $id, $formattedData['type'], 'type')) {
+            ModelService::update(RefIdentityType::class, $id, $formattedData);
+            $this->reset('type', 'description');
             $this->openModal = false;
         } else {
-            $this->addError('type', 'The ID has already been taken.');
+            $this->addError('type', 'The type has already been taken.');
         }
     }
 
     public function delete($id,$type)
     {
-    $this->popupService->confirm($this, 'ConfirmDelete', 'Delete the information?', "Are you sure you want to delete TYPE: ".$type."?", $id);
+        $this->popupService->confirm($this, 'ConfirmDelete', 'Delete the information?', "Are you sure you want to delete TYPE: ".$type."?", $id);
     }
 
     public function ConfirmDelete($id)
     {
-        IdentityTypeService::deleteIdentityType($id);
+        ModelService::delete(RefIdentityType::class, $id);
     }
 
     public function render()
     {
-        $data = $this->identitytypeService->getIdentityTypeResult($this->searchQuery, $this->paginated);
+        $data = MaintenanceService::getPaginated(
+            RefIdentityType::class,
+            $this->paginated, // $perPage
+            $this->searchQuery, // $searchQuery
+        );
 
         return view('livewire.admin.maintenance.identity-type', [
             'data' => $data,
