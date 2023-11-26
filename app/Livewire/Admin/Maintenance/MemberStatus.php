056f50060aff4abb018edee5c3a2f2642e667e9c
Nazirul Fitri <nazirul@csc.net.my>
2023-11-26 20:39:49 +0800

    Refactor Member Status Maintenance

--- app/Livewire/Admin/Maintenance/MemberStatus.php
+++ app/Livewire/Admin/Maintenance/MemberStatus.php
@@ -3,10 +3,12 @@
 namespace App\Livewire\Admin\Maintenance;
 
 use App\Models\Ref\RefMemStatus;
+use App\Rules\Maintenance\ValidDescription;
+use App\Services\General\ModelService;
 use App\Services\General\PopupService;
-use App\Services\Model\MemberStatusService;
+use App\Services\Maintenance\FormattingService;
+use App\Services\Maintenance\GeneralService as MaintenanceService;
 use App\Traits\MaintenanceModalTrait;
-use Livewire\Attributes\Rule;
 use Livewire\Component;
 use Livewire\WithPagination;
 use WireUi\Traits\Actions;
@@ -15,27 +17,34 @@ class MemberStatus extends Component
 {
     use Actions, WithPagination,MaintenanceModalTrait;
 
-    #[Rule('required|alpha|size:2','member status')]
-    public $mbr_status;
-
-    #[Rule('required|regex:/^[A-Za-z\s]+$/')]
-    public $description;
-
+    // Properties for modal and member status management
     public $openModal;
     public $modalTitle;
     public $modalDescription;
     public $modalMethod;
     public $mbrstatus;
+
+    // Properties for member status data
+    public $mbr_status;
+    public $description;
+
+    // Pagination and searching
     public $paginated;
     public $searchQuery;
 
-    protected $memberstatus_Service;
+    // Services
     protected $popupService;
 
+    public function rules()
+    {
+        return [
+            'mbr_status' => ['required', 'alpha', 'max:1'],
+            'description' => ['required', new ValidDescription],
+        ];
+    }
 
     public function __construct()
     {
-        $this->memberstatus_Service = new MemberStatusService();
         $this->popupService = app(PopupService::class);
     }
 
@@ -46,42 +55,47 @@ class MemberStatus extends Component
         $this->resetValidation();
     }
 
-    public function openUpdateModal($mbr_status)
+    public function openUpdateModal($id)
     {
-        $this->mbrstatus = RefMemStatus::find($mbr_status);
+        $this->mbrstatus = ModelService::findById(RefMemStatus::class, $id);
         $this->mbr_status = $this->mbrstatus->mbr_status;
         $this->description = $this->mbrstatus->description;
-        $this->setupModal("update", "Update Member Status", "Description", "update({$mbr_status})");
+        $this->setupModal("update", "Update Member Status", "Description", "update({$id})");
         $this->resetValidation();
+    }
+
+    protected function formatData()
+    {
+        return [
+            'mbr_status' => FormattingService::formatCode($this->mbr_status),
+            'description' => FormattingService::formatDescription($this->description),
+        ];
     }
 
     public function create()
     {
         $this->validate();
 
-        if (MemberStatusService::isCodeExists($this->mbr_status)) {
+        $formattedData = $this->formatData();
+
+        if (MaintenanceService::isCodeExists(RefMemStatus::class, $formattedData['mbr_status'], 'mbr_status')) {
             $this->addError('mbr_status', 'The code has already been taken.');
         } else {
-            $data = [
-                'description' => trim(preg_replace('/\s+/', ' ', strtoupper($this->description))),
-                'mbr_status' => trim(strtoupper($this->mbr_status)),
-            ];
-            MemberStatusService::createMemberStatusService($data);
-            $this->reset();
+            ModelService::create(RefMemStatus::class, $formattedData);
+            $this->reset('mbr_status', 'description');
             $this->openModal = false;
         }
     }
-    
+
     public function update($id)
     {
         $this->validate();
 
-        if (MemberStatusService::canUpdateCode($id, $this->mbr_status)) {
-            $data = [
-                'description' => trim(preg_replace('/\s+/', ' ', strtoupper($this->description))),
-                'mbr_status' => trim(strtoupper($this->mbr_status)),
-            ];
-            MemberStatusService::updateMemberStatusService($id, $data);
+        $formattedData = $this->formatData();
+
+        if (MaintenanceService::canUpdateCode(RefMemStatus::class, $id, $formattedData['mbr_status'], 'mbr_status')) {
+            ModelService::update(RefMemStatus::class, $id, $formattedData);
+            $this->reset('mbr_status', 'description');
             $this->openModal = false;
         } else {
             $this->addError('mbr_status', 'The code has already been taken.');
@@ -95,12 +109,17 @@ class MemberStatus extends Component
 
     public function ConfirmDelete($id)
     {
-        MemberStatusService::deleteMemberStatusService($id);
+        ModelService::delete(RefMemStatus::class, $id);
     }
-    
+
     public function render()
     {
-        $data = $this->memberstatus_Service->getMemStatusResult($this->searchQuery, $this->paginated);
+        $data = MaintenanceService::getPaginated(
+            RefMemStatus::class,
+            $this->paginated, // $perPage
+            $this->searchQuery, // $searchQuery
+        );
+
         return view('livewire.admin.maintenance.member-status',[
             'data' =>$data,
         ])->extends('layouts.main');
