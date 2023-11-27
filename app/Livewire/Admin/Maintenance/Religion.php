9093e1796183c0da9a73f8c038dbff2a493ab6b5
Nazirul Fitri <nazirul@csc.net.my>
2023-11-27 12:35:49 +0800

    Refactor Takaful Maintenance

--- app/Livewire/Admin/Maintenance/Religion.php
+++ app/Livewire/Admin/Maintenance/Religion.php
@@ -24,7 +24,7 @@ class Religion extends Component
     public $modalMethod;
     public $religion;
 
-    // Properties for relationship data
+    // Properties for religion data
     public $code;
     public $description;
 
