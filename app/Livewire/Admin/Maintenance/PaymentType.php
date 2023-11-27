f0cf942d8803d94a23f9e06017e95e2b4e3cb97e
Nazirul Fitri <nazirul@csc.net.my>
2023-11-27 13:26:57 +0800

    Refactor Payment Type Maintenance

--- app/Livewire/Admin/Maintenance/PaymentType.php
+++ app/Livewire/Admin/Maintenance/PaymentType.php
@@ -3,10 +3,12 @@
 namespace App\Livewire\Admin\Maintenance;
 
 use App\Models\Ref\RefPaymentType;
+use App\Rules\Maintenance\ValidDescription;
+use App\Services\General\ModelService;
 use App\Services\General\PopupService;
-use App\Services\Model\PaymentTypeService;
+use App\Services\Maintenance\FormattingService;
+use App\Services\Maintenance\GeneralService as MaintenanceService;
 use App\Traits\MaintenanceModalTrait;
-use Livewire\Attributes\Rule;
 use Livewire\Component;
 use Livewire\WithPagination;
 use WireUi\Traits\Actions;
@@ -15,25 +17,32 @@ class PaymentType extends Component
 {
     use Actions, WithPagination,MaintenanceModalTrait;
 
-    #[Rule('required|regex:/^[A-Za-z\s\/]*$/|max:50')]
-    public $description;
-    
+    // Properties for modal and payment type management
     public $openModal;
     public $modalTitle;
     public $modalDescription;
     public $modalMethod;
     public $paymentType;
+
+    // Properties for cust payment type data
+    public $description;
+
+    // Pagination & searching
     public $paginated;
     public $searchQuery;
-    public $priority;
 
-    protected $paymentType_Service;
+    // Services
     protected $popupService;
 
+    public function rules()
+    {
+        return [
+            'description' => ['required', new ValidDescription, 'max:50'],
+        ];
+    }
 
     public function __construct()
     {
-        $this->paymentType_Service = new PaymentTypeService();
         $this->popupService = app(PopupService::class);
     }
 
@@ -46,40 +55,46 @@ class PaymentType extends Component
 
     public function openUpdateModal($description)
     {
-        $this->paymentType = RefPaymentType::find($description);
+        $this->paymentType = ModelService::findById(RefPaymentType::class, $description);
         $this->description = $this->paymentType->description;
         $this->setupModal("update", "Update Payment Type", "Description", "update({$description})");
         $this->resetValidation();
     }
 
+    protected function formatData()
+    {
+        return [
+            'description' => FormattingService::formatDescription($this->description),
+        ];
+    }
+
     public function create()
     {
         $this->validate();
-        
-        if (PaymentTypeService::isCodeExists($this->description)) {
-            $this->addError('description', 'The status has already been taken.');
+
+        $formattedData = $this->formatData();
+
+        if (MaintenanceService::isCodeExists(RefPaymentType::class, $formattedData['description'], 'description')) {
+            $this->addError('description', 'The type has already been taken.');
         } else {
-            $data = [
-                'description'=> trim(preg_replace('/\s+/', ' ', strtoupper($this->description))),
-            ];
-            PaymentTypeService::createPaymentType($data);
-            $this->reset();
+            ModelService::create(RefPaymentType::class, $formattedData);
+            $this->reset('description');
             $this->openModal = false;
         }
     }
-    
+
     public function update($id)
     {
         $this->validate();
 
-        if (PaymentTypeService::canUpdatePaymentType($id, $this->description)){
-            $data = [
-                'description'=> trim(preg_replace('/\s+/', ' ', strtoupper($this->description))),
-            ];
-            PaymentTypeService::updatePaymentType($id, $data);
+        $formattedData = $this->formatData();
+
+        if (MaintenanceService::canUpdateCode(RefPaymentType::class, $id, $formattedData['description'], 'description')) {
+            ModelService::update(RefPaymentType::class, $id, $formattedData);
+            $this->reset('description');
             $this->openModal = false;
         } else {
-            $this->addError('description', 'The status has already been taken.');
+            $this->addError('description', 'The type has already been taken.');
         }
     }
 
@@ -90,13 +105,20 @@ class PaymentType extends Component
 
     public function ConfirmDelete($id)
     {
-        PaymentTypeService::deletePaymentType($id);
-
+        ModelService::delete(RefPaymentType::class, $id);
     }
-    
+
     public function render()
     {
-        $data = $this->paymentType_Service->getPaymentTypeResult($this->searchQuery, $this->paginated);
+        $data = MaintenanceService::getPaginated(
+            RefPaymentType::class,
+            $this->paginated, // $perPage
+            $this->searchQuery, // $searchQuery
+            [
+                'description' => 'ASC'
+            ]
+        );
+
         return view('livewire.admin.maintenance.payment-type',[
             'data' =>$data,
         ])->extends('layouts.main');
