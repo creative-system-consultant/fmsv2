469c5bd8632018b7935b15f5e244aaabba088879
wan <47422904+safwanAzman@users.noreply.github.com>
2023-11-28 16:02:52 +0800

    feat: add permission other info

---
+++ config/module/other-info/tab/index.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Other Info - Tab Btn
+    |--------------------------------------------------------------------------
+    */
+
+    [
+        'permission' => 'finance tab',
+        'index' => '0',
+        'icon' => 'currency-dollar',
+        'name' => 'Finance',
+    ],
+    [
+        'permission' => 'third party info tab',
+        'index' => '1',
+        'icon' => 'information-circle',
+        'name' => 'Third Party Info',
+    ],
+    [
+        'permission' => 'guarantee/guarantor tab',
+        'index' => '2',
+        'icon' => 'shield-check',
+        'name' => 'Guarantee/Guarantor',
+    ],
+];
--- resources/views/livewire/cif/other/index.blade.php
+++ resources/views/livewire/cif/other/index.blade.php
@@ -4,41 +4,46 @@
         @include('misc.loading')
     </div>
     <x-container title="Others Information" routeBackBtn="" titleBackBtn="" disableBackBtn="false">
-        <div x-data="{ active:0 }" class="relative">
+        <div x-data="{ active: @entangle('setIndex') }" class="relative">
             <div class=" bg-white border rounded-lg shadow-md w-full dark:bg-gray-800 dark:text-gray-400 dark:border-gray-700 ">
                 <div class="flex flex-wrap justify-start sm:justify-start">
-                    <x-hovertab.title name="0" wire:click="setState(0)">
-                        <x-icon name="currency-dollar" class="w-6 h-6 mr-2"/>
-                        <span class="text-sm tooltip-text bg-primary-500 border rounded border-primary-500 text-white -mt-14">
-                            Finance
-                        </span>
-                    </x-hovertab.title>
-
-                    <x-hovertab.title name="1" wire:click="setState(1)">
-                        <x-icon name="information-circle" class="w-6 h-6 mr-2"/>
-                        <span class="text-sm tooltip-text bg-primary-500 border rounded border-primary-500 text-white -mt-14">
-                            Third Party Info
-                        </span>
-                    </x-hovertab.title>
-
-                    <x-hovertab.title name="2" wire:click="setState(2)">
-                        <x-icon name="shield-check" class="w-6 h-6 mr-2"/>
-                        <span class="text-sm tooltip-text bg-primary-500 border rounded border-primary-500 text-white -mt-14">
-                            Guarantee/Guarantor
-                        </span>
-                    </x-hovertab.title>
+                    @foreach(config('module.other-info.tab.index') as $config)
+                        @php
+                            $hasPermission = auth()->check() && auth()->user()->hasClientSpecificPermission($config['permission'], auth()->user()->client_id);
+                        @endphp
+                        @if($hasPermission)
+                            <x-hovertab.title name="{{ $config['index'] }}" wire:click="setState({{ $config['index'] }})">
+                                <x-icon name="{{ $config['icon'] }}" class="w-6 h-6 mr-2"/>
+                                <span class="text-sm tooltip-text bg-primary-500 border rounded border-primary-500 text-white -mt-14">
+                                    {{ $config['name'] }}
+                                </span>
+                            </x-hovertab.title>
+                        @endif
+                    @endforeach
                 </div>
-
             </div>
 
             <div class="mt-12">
-                @if($setIndex  == '0')
-                    <livewire:cif.info.finance :uuid="$uuid" />
-                @elseif($setIndex  == '1')
-                    <livewire:cif.info.third-party-info :uuid="$uuid" />
-                @elseif($setIndex  == '2')
-                <livewire:cif.info.guarantee :uuid="$uuid" />
-                @endif
+                @foreach(config('module.other-info.tab.index') as $config)
+                @php
+                    $hasPermission = auth()->check() && auth()->user()->hasClientSpecificPermission($config['permission'], auth()->user()->client_id);
+                @endphp
+                @if($setIndex == $config['index'])
+                    @if($hasPermission)
+                        @switch($setIndex)
+                            @case('0')
+                                <livewire:cif.info.finance :uuid="$uuid" />
+                                @break
+                            @case('1')
+                                <livewire:cif.info.third-party-info :uuid="$uuid" />
+                                @break
+                            @case('2')
+                                <livewire:cif.info.guarantee :uuid="$uuid" />
+                                @break
+                            @endswitch
+                        @endif
+                    @endif
+                @endforeach
             </div>
         </div>
     </x-container>
