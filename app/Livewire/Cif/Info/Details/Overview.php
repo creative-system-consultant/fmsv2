079ab87bf15680e9ffbc70676c4aeabefd999753
Nazirul Fitri <nazirul@csc.net.my>
2024-01-30 10:55:06 +0800

    feat: cif edit feature #2

--- app/Livewire/Cif/Info/Details/Overview.php
+++ app/Livewire/Cif/Info/Details/Overview.php
@@ -16,35 +16,45 @@ class Overview extends Component
 {
     public $disabled = true;
     public $customerInfo;
+    public $membershipInfo;
     public $uuid, $staff_no, $ref_no, $name, $identity_no, $cust_status, $status_id, $apply_date, $start_date, $status_change_date, $approved_retirement_date, $effective_retirement_date, $entrance_fee, $entrance_fee_date, $bank_id, $bank_acct_no, $payment_type, $staffno_payer, $va_account, $clientID;
+    public $banks;
+    public $bankOptions = [];
 
     public function mount()
     {
         $this->clientID = auth()->user()->client_id;
+        $this->banks = RefBank::where('client_id', $this->clientID)->get();
+
+        // Transform banks data to match the select options format
+        $this->bankOptions = $this->banks->map(function ($bank) {
+            return ['name' => $bank->description, 'id' => $bank->id];
+        })->toArray();
+
         $this->customerInfo = CifCustomer::where('uuid', $this->uuid)->where('client_id', $this->clientID)->first();
-        $membershipInfo = Membership::where('cif_id', $this->customerInfo->id)->first();
-        $memberStatus = RefMemStatus::select('description')->where('mbr_status', $membershipInfo->mbr_status)->first();
+        $this->membershipInfo = Membership::where('cif_id', $this->customerInfo->id)->first();
+        $memberStatus = RefMemStatus::select('description')->where('mbr_status', $this->membershipInfo->mbr_status)->first();
         $bank_name = RefBank::select('description')->where('id',  $this->customerInfo->bank_id)->first();
-        $paymentType = RefPaymentType::select('description')->where('id', $membershipInfo->payment_type)->first();
+        $paymentType = RefPaymentType::select('description')->where('id', $this->membershipInfo->payment_type)->first();
 
         $this->staff_no = $this->customerInfo->staff_no;
-        $this->ref_no = $membershipInfo->mbr_no;
+        $this->ref_no = $this->membershipInfo->mbr_no;
         $this->name = $this->customerInfo->name;
         $this->identity_no = $this->customerInfo->identity_no;
         $this->cust_status = ($this->customerInfo->cust_status == 'Y' ? 'Members' : 'Non Members');
         $this->status_id = $memberStatus->description;
-        $this->apply_date = $membershipInfo->apply_date;
-        $this->start_date = $membershipInfo->start_date;
-        $this->status_change_date = $membershipInfo->status_change_date;
-        $this->approved_retirement_date = $membershipInfo->approved_retirement_date;
-        $this->effective_retirement_date = $membershipInfo->effective_retirement_date;
-        $this->entrance_fee = $membershipInfo->entrance_fee;
-        $this->entrance_fee_date = $membershipInfo->entrance_fee_date;
-        $this->bank_id = $bank_name->description ?? '';
+        $this->apply_date = $this->membershipInfo->apply_date;
+        $this->start_date = $this->membershipInfo->start_date;
+        $this->status_change_date = $this->membershipInfo->status_change_date;
+        $this->approved_retirement_date = $this->membershipInfo->approved_retirement_date;
+        $this->effective_retirement_date = $this->membershipInfo->effective_retirement_date;
+        $this->entrance_fee = $this->membershipInfo->entrance_fee;
+        $this->entrance_fee_date = $this->membershipInfo->entrance_fee_date;
+        $this->bank_id = $this->customerInfo->bank_id;
         $this->bank_acct_no = $this->customerInfo->bank_acct_no;
         $this->payment_type = $paymentType->description ?? '';
         $this->staffno_payer = $this->customerInfo->staffno_payer;
-        $this->va_account = $membershipInfo->va_account;
+        $this->va_account = $this->membershipInfo->va_account;
     }
 
     #[On('edit')]
@@ -56,8 +66,17 @@ class Overview extends Component
     #[On('save')]
     public function saveData()
     {
+        // dd($this->bank_id);
         $this->customerInfo->update([
-            'staff_no' => $this->staff_no
+            'name' => $this->name,
+            'identity_no' => $this->identity_no,
+            'bank_id' => (int) $this->bank_id,
+            'bank_acct_no' => $this->bank_acct_no
+        ]);
+
+        $this->membershipInfo->update([
+            'approved_retirement_date' => $this->approved_retirement_date,
+            'effective_retirement_date' => $this->effective_retirement_date,
         ]);
 
         $this->disabled = true;
--- resources/views/livewire/cif/info/details/overview.blade.php
+++ resources/views/livewire/cif/info/details/overview.blade.php
@@ -2,24 +2,24 @@
     <div>
         <x-card title="Membership Overview" >
             <div class="grid grid-cols-1 gap-2 md:grid-cols-3">
-                <x-input label="Staff No" placeholder="" wire:model="staff_no" :disabled=$disabled />
-                <x-input label="Membership No." placeholder="" wire:model="ref_no" :disabled=$disabled />
+                <x-input label="Staff No" placeholder="" wire:model="staff_no" :disabled="true" />
+                <x-input label="Membership No." placeholder="" wire:model="ref_no" :disabled="true" />
                 <x-input label="Name" placeholder="" wire:model="name" :disabled=$disabled />
                 <x-input label="Identity No" placeholder="" wire:model="identity_no" :disabled=$disabled />
-                <x-input label="Category" placeholder="" wire:model="cust_status" :disabled=$disabled />
-                <x-input label="Status" placeholder="" wire:model="status_id" :disabled=$disabled />
-                <x-input label="Apply Date" placeholder="" wire:model="apply_date" :disabled=$disabled />
-                <x-input label="Start Date" placeholder="" wire:model="start_date" :disabled=$disabled />
-                <x-input label="Status Change Date" placeholder="" wire:model="status_change_date" :disabled=$disabled />
+                <x-input label="Category" placeholder="" wire:model="cust_status" :disabled="true" />
+                <x-input label="Status" placeholder="" wire:model="status_id" :disabled="true" />
+                <x-input label="Apply Date" placeholder="" wire:model="apply_date" :disabled="true" />
+                <x-input label="Start Date" placeholder="" wire:model="start_date" :disabled="true" />
+                <x-input label="Status Change Date" placeholder="" wire:model="status_change_date" :disabled="true" />
                 <x-input label="Approved Retirement Date" placeholder="" wire:model="approved_retirement_date" :disabled=$disabled />
                 <x-input label="Effective Retirement Date" placeholder="" wire:model="effective_retirement_date" :disabled=$disabled />
-                <x-input label="Entrance Fee" placeholder="" wire:model="entrance_fee" :disabled=$disabled />
-                <x-input label="Entrance Fee Date" placeholder="" wire:model="entrance_fee_date" :disabled=$disabled />
-                <x-input label="Bank ID" placeholder="" wire:model="bank_id" :disabled=$disabled />
+                <x-input label="Entrance Fee" placeholder="" wire:model="entrance_fee" :disabled="true" />
+                <x-input label="Entrance Fee Date" placeholder="" wire:model="entrance_fee_date" :disabled="true" />
+                <x-native-select label="Select Bank" :options="$bankOptions" option-label="name" option-value="id" wire:model="bank_id" :disabled=$disabled />
                 <x-input label="Bank Acct No" placeholder="" wire:model="bank_acct_no" :disabled=$disabled />
                 <x-input label="Payment Type" placeholder="" wire:model="payment_type" :disabled=$disabled />
                 <x-input label="Staff No (Payer)" placeholder="" wire:model="staffno_payer" :disabled=$disabled />
-                <x-input label="VA Account" placeholder="" wire:model="va_account" :disabled=$disabled />
+                <x-input label="VA Account" placeholder="" wire:model="va_account" :disabled="true" />
             </div>
         </x-card>
     </div>
