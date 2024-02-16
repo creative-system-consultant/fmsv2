9a0ab5de7a334f5ed7df6ad7e287cb073b9efd02
nazirulfitrifauzi <55203232+nazirulfitrifauzi@users.noreply.github.com>
2024-02-16 12:14:46 +0800

    Update employer.blade.php
    removed unused field

--- resources/views/livewire/cif/info/employer.blade.php
+++ resources/views/livewire/cif/info/employer.blade.php
@@ -8,7 +8,6 @@
                 <x-input label="Phone No" placeholder="" wire:model="company_phone_no" :disabled=$disabled />
                 <x-input label="Work Phone No" placeholder="" wire:model="company_fax_no" :disabled=$disabled />
                 <x-input label="Department" placeholder="" wire:model="job_department" :disabled=$disabled />
-                {{-- <x-input label="Job Status ID" placeholder="" wire:model="job_status_id" :disabled=$disabled /> --}}
                 <x-input label="Position" placeholder="" wire:model="job_position" :disabled=$disabled />
                 <x-input label="Start Employment Date" placeholder="" wire:model="current_employment_date" :disabled=$disabled />
                 <x-input label="Salary" placeholder="" wire:model="current_salary" :disabled=$disabled />
