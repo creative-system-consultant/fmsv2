f1081ead88330dcf5263e2a4f76037a7ad01d5ed
Nazirul Fitri <nazirul@csc.net.my>
2024-02-22 15:48:36 +0800

    fix: show and hide based on excel sheet20

--- resources/views/livewire/cif/info/employer.blade.php
+++ resources/views/livewire/cif/info/employer.blade.php
@@ -5,12 +5,13 @@
             <div class="grid grid-cols-1 gap-2 md:grid-cols-3">
                 <x-input label="Name" placeholder="" wire:model="current_employer_name" :disabled=$disabled />
                 <x-input label="Address" placeholder="" wire:model="company_address" :disabled=$disabled />
-                <x-input label="Phone No" placeholder="" wire:model="company_phone_no" :disabled=$disabled />
-                <x-input label="Work Phone No" placeholder="" wire:model="company_fax_no" :disabled=$disabled />
+                <x-input label="Office Phone" placeholder="" wire:model="company_phone_no" :disabled=$disabled />
+                <x-input label="Work Phone (Opt)" placeholder="" wire:model="company_fax_no" :disabled=$disabled />
                 <x-input label="Department" placeholder="" wire:model="job_department" :disabled=$disabled />
                 <x-input label="Position" placeholder="" wire:model="job_position" :disabled=$disabled />
+                <x-input label="Work Status" placeholder="" wire:model="work_status" :disabled=$disabled />
+                <x-input label="Gross Salary" placeholder="" wire:model="current_salary" :disabled=$disabled />
                 <x-input label="Start Employment Date" placeholder="" wire:model="current_employment_date" :disabled=$disabled />
-                <x-input label="Salary" placeholder="" wire:model="current_salary" :disabled=$disabled />
             </div>
         </x-card>
     </div>
